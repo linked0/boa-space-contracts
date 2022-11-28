0c2c3a2fc9f1c8fa35f8594c80eb28ed82e55a23
Hyunjae Lee <linked0@gmail.com>
2022-11-28 16:04:52 +0900

    Script for adding a SharedProxyAddress

---
+++ script/finpl/add_shared_proxy.ts
@@ -0,0 +1,29 @@
+import { NonceManager } from "@ethersproject/experimental";
+import { Wallet } from "ethers";
+import { ethers } from "hardhat";
+import { GasPriceManager } from "../../utils/GasPriceManager";
+import { AssetContractShared } from "../../typechain-types";
+
+async function main() {
+    const AssetContractFactory = await ethers.getContractFactory("AssetContractShared");
+    const provider = ethers.provider;
+
+    const admin = new Wallet(process.env.ADMIN_KEY || "");
+    const adminSigner = new NonceManager(new GasPriceManager(provider.getSigner(admin.address)));
+    const proxy = new Wallet(process.env.SHARED_PROXY_KEY || "");
+
+    const assetContract = await AssetContractFactory.attach(
+        process.env.ASSET_CONTRACT_SHARED_ADDRESS || ""
+    );
+    const ownerContract = await assetContract.connect(adminSigner);
+
+    await ownerContract.addSharedProxyAddress(proxy.address);
+    console.log("Add to SharedProxyAddress - address:", proxy.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
