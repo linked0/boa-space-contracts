a207c0a5bde05517814a02c994316ecc2f07e96a
Hyunjae Lee <linked0@gmail.com>
2023-01-06 16:24:43 +0900

    Set Seaport and Conduit on the SharedStorefrontLazyMintAdapter
    Our Seaport and Conduit addresses are not constant so
    we should set the addresses through the constructor of
    the `SharedStorefrontLazyMintAdapter` contract.

--- script/deploy_lazymint_adapter.ts
+++ script/deploy_lazymint_adapter.ts
@@ -5,15 +5,27 @@ import { GasPriceManager } from "../utils/GasPriceManager";
 
 async function main() {
     const LazymintAdapterFactory = await ethers.getContractFactory("SharedStorefrontLazyMintAdapter");
+    const ConduitControllerFactory = await ethers.getContractFactory("ConduitController");
     const provider = ethers.provider;
 
     const admin = new Wallet(process.env.ADMIN_KEY || "");
     const adminSigner = new NonceManager(new GasPriceManager(provider.getSigner(admin.address)));
 
-    const tokenAddress = process.env.ASSET_CONTRACT_SHARED_ADDRESS || "";
-    const lazymintAdapter = await LazymintAdapterFactory.connect(adminSigner).deploy(tokenAddress);
-    await lazymintAdapter.deployed();
+    const marketplaceAddress = process.env.SEAPORT_ADDRESS;
 
+    const conduitContorller = await ConduitControllerFactory.attach(process.env.CONDUIT_CONTROLLER_ADDRESS || "");
+    const ownerConduitContorller = await conduitContorller.connect(adminSigner);
+    const conduitKey = process.env.CONDUIT_KEY || "";
+    const { conduit: conduitAddress, exists } = await ownerConduitContorller.getConduit(conduitKey);
+    console.log("seaportAddress: %s, conduitAddress: %s", marketplaceAddress, conduitAddress);
+
+    const tokenAddress = process.env.ASSET_CONTRACT_SHARED_ADDRESS || "";
+    const lazymintAdapter = await LazymintAdapterFactory.connect(adminSigner).deploy(
+        marketplaceAddress,
+        conduitAddress,
+        tokenAddress
+    );
+    await lazymintAdapter.deployed();
     console.log("SharedStorefrontLazyMintAdapter - deployed to:", lazymintAdapter.address);
 }
 
